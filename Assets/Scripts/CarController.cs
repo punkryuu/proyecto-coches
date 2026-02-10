c94322b047dec1eb61dbbc2588c70c512df8e8a4
rodudi <rodrigodudi2005@gmail.com>
2026-02-11 00:33:23 +0100

    drift añadido
    falta testear bien y añadir turbo

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,16 +8,21 @@ public class CarController : MonoBehaviour
     [SerializeField] Rigidbody sphere;
     [SerializeField] InputActionReference accelerateInput;
     [SerializeField] InputActionReference steerInput;
+    [SerializeField] InputActionReference driftInput;
 
     float speed, currentSpeed;
     float rotate, currentRotate;
-
+    bool drifting = false;
+    sbyte driftDir;
+    float driftPower;
 
     // estos son los paremetros que pillaria luego de los SO de los personajes
     float maxSpeed = 40;
-    float acceleration = 100f;
-    float steering = 50f;
-    float gravity = 100f;
+    float acceleration = 100;
+    float steering = 50;
+    float gravity = 100;
+    float driftControl = 1;
+    float turbo =1;
     Vector3 ajustePosicionCoche=new Vector3(0.1f, 0.3f, 0);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -34,18 +40,54 @@ public class CarController : MonoBehaviour
         //acelerar
         if (accelerateInput.action.IsPressed())
             speed = acceleration;
-       
+
+
         //girar
-        float s = steerInput.action.ReadValue<float>();
-        if (s != 0)
+        float horizontal = steerInput.action.ReadValue<float>();
+        if (horizontal != 0 )
         {
-            int dir;
-            if (s > 0)
+            sbyte dir;
+            if (horizontal > 0)
                 dir = 1;
             else dir = -1;
-            float amount = Mathf.Abs(s);
+            float amount = Mathf.Abs(horizontal);
             Steer(dir, amount);
-             
+           
+        }
+        // derrapar
+
+        if (driftInput.action.IsPressed() && !drifting && horizontal != 0)
+        {
+            drifting = true;
+            if (horizontal > 0)
+                driftDir = 1;
+            else driftDir = -1;
+        }
+        if (drifting) 
+        {
+            //this float value, float from1, float to1, float from2, float to2
+            //(value - from1) / (to1 - from1) * (to2 - from2) + from2;
+            float control;
+            float powerControl;
+            if (driftDir == 1)
+            {
+                control = Remap(horizontal,-1, 1, 0, 2);
+                powerControl = Remap(horizontal, -1, 1, .2f, 1);
+            }
+            else
+            {
+                control = Remap(horizontal, -1, 1, 2, 0);
+                powerControl = Remap(horizontal, -1, 1, 1, .2f);
+            }
+      
+
+            Steer(driftDir, control);
+            driftPower += powerControl*turbo;
+        }
+        if (!driftInput.action.IsPressed() && drifting)
+        {
+            drifting = false;
+            driftPower = 0;
         }
         currentSpeed = Mathf.SmoothStep(currentSpeed, speed, Time.deltaTime * 12f);
         speed = 0f;
@@ -64,12 +106,18 @@ public class CarController : MonoBehaviour
 
         // palante
         if (sphere.linearVelocity.magnitude < maxSpeed)
-            sphere.GetComponent<Rigidbody>().AddForce(kartModel.transform.forward * currentSpeed, ForceMode.Acceleration);
+        {
+            if (!drifting)
+                sphere.AddForce(kartModel.transform.forward * currentSpeed, ForceMode.Acceleration);
+            else
+                sphere.AddForce(transform.forward * currentSpeed, ForceMode.Acceleration);
+
+        }
         Debug.Log(sphere.linearVelocity.magnitude);
         // ahora caigo
         if (!Physics.Raycast(transform.position, Vector3.down, 3))
         {
-            sphere.GetComponent<Rigidbody>().AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+            sphere.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
         }
         // girar
         transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.deltaTime * 5f);
@@ -77,8 +125,15 @@ public class CarController : MonoBehaviour
         kartModel.parent.up = Vector3.Lerp(kartModel.parent.up, hitNear.normal, Time.deltaTime*8f);
         kartModel.parent.Rotate(0, transform.eulerAngles.y, 0);
     }
-    public void Steer(int direction, float amount) 
+    public void Steer(sbyte direction, float amount) 
     {
-        rotate = (steering * direction) * amount;
+        if (!drifting)
+            rotate = (steering * direction) * amount;
+        else
+            rotate = (driftControl * direction) * amount;
+    }
+    public float Remap(float value, float from1, float to1, float from2, float to2) 
+    { 
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
     }
 }
