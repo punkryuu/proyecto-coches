f7f7e8e664021700b08ac1d77e2d9556e320f799
rodudi <rodrigodudi2005@gmail.com>
2026-02-18 19:55:10 +0100

    barra funciona
    he cambiado el OntriggerEnter al cilindro y añadido la etiqueta player al colider del kart, he añadido tambien los serializefield necesarios para hacerlo funcionable y puesto una variable publica que hay que cmabiarla a un get set

--- Assets/Scripts/RotacionObjeto.cs
+++ Assets/Scripts/RotacionObjeto.cs
@@ -1,10 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RotacionObjeto : MonoBehaviour
 {
-   public Vector3 velocidadRotacion = new Vector3(0, 100, 0); 
+    [SerializeField] Slider barraPoder;
+    [SerializeField] UIManager uiManager;
+    public Vector3 velocidadRotacion = new Vector3(0, 100, 0); 
     void Update()
     {
         transform.Rotate(velocidadRotacion * Time.deltaTime); 
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            barraPoder.value += uiManager.progreso;
+            Debug.Log("Detectado!");
+        }
+        if (barraPoder.value > barraPoder.maxValue)
+            barraPoder.value = barraPoder.maxValue;
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,20 +6,11 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] CarController car; 
     [SerializeField] TextMeshProUGUI textoVelocidad;
-    [SerializeField] Slider barraPoder;
-    float progreso = 10;   
+
+    public float progreso = 10;   //prueba, cambiar el public a un get set
     float speed = 0;
 
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            barraPoder.value+= progreso;
-            Debug.Log("Detectado!");
-        }
-        if(barraPoder.value > barraPoder.maxValue)
-            barraPoder.value = barraPoder.maxValue;
-    }
+    
 
     private void Update()
     {
