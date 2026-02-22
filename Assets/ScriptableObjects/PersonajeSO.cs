acba1e16d923d6146976696f4062d5edfa086a96
rodudi <rodrigodudi2005@gmail.com>
2026-02-22 19:22:22 +0100

    Update PersonajeSO.cs

--- Assets/ScriptableObjects/PersonajeSO.cs
+++ Assets/ScriptableObjects/PersonajeSO.cs
@@ -4,6 +4,19 @@ using UnityEngine.UI;
 [CreateAssetMenu(fileName = "PersonajeSO", menuName = "Scriptable Objects/PersonajeSO")]
 public class PersonajeSO : ScriptableObject
 {
+    public Transform visual;
+    public Transform driftParticles;
+    public Transform turboParticles;
     public GameObject characterPrefab;
     public Sprite selectionImage;
+    public float maxSpeedMultiplier = 1f;
+    public float accelerationMultiplier = 1f;
+    public float steeringMultiplier = 1f;
+    public float weightMultiplier = 1f;
+    public float driftControlMultiplier = 1f;
+    public float turboMultiplier = 1f;
+    public float airControlMultiplier = 1f;
+    public float verticalOffset = 0f;
+    public float horizontalOffset = 0f;
+
 }
